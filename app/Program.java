1c2730c7a4dffde43731489aa5524369d4828a27
gustcustodio <gustcustodio@gmail.com>
2025-04-24 06:52:33 -0300

    chore: mudança de diretório

---
+++ app/Program.java
@@ -0,0 +1,11 @@
+package app;
+
+import app.menu.MainMenu;
+
+public class Program {
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+
+        menu.initialMenu();
+    }
+}
