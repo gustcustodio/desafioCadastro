a0ff45f2fd51fa46012d4e632817ac9171c907a3
gustcustodio <gustcustodio@gmail.com>
2025-04-24 06:54:08 -0300

    feat: implementação inicial do menu principal do sistema

---
+++ app/menu/MainMenu.java
@@ -0,0 +1,28 @@
+package app.menu;
+
+import java.io.*;
+
+public class MainMenu {
+    public void initialMenu() {
+        System.out.println("1. Cadastrar um novo pet");
+        System.out.println("2. Alterar os dados do pet cadastrado");
+        System.out.println("3. Deletar um pet cadastrado");
+        System.out.println("4. Listar todos os pets cadastrados");
+        System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+        System.out.println("6. Sair");
+    }
+
+    public void formReader() {
+        String path = "app/formulario.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
