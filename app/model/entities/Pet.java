01f73aae4cf881bc41f039ff4f01bbad0c7794cb
gustcustodio <gustcustodio@gmail.com>
2025-04-24 06:54:43 -0300

    feat: criação da classe Pet

---
+++ app/model/entities/Pet.java
@@ -0,0 +1,13 @@
+package app.model.entities;
+
+import app.model.enums.Sex;
+import app.model.enums.Type;
+
+public class Pet {
+    private Double age, weight;
+    private Integer numberOfAddress;
+    private String name, cityOfAdress, streetOfAdress, breed;
+
+    private Sex sex;
+    private Type type;
+}
