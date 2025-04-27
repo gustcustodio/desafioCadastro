7afad089958bd57ba0b4459bfeece433c5cd2fd5
gustcustodio <gustcustodio@gmail.com>
2025-04-27 09:27:11 -0300

    feat: criação do construtor e dos getters e setters da classe Pet

--- app/model/entities/Pet.java
+++ app/model/entities/Pet.java
@@ -10,4 +10,76 @@ public class Pet {
 
     private Sex sex;
     private Type type;
+
+    public Pet() {
+    }
+
+    public Pet(String name, Type type, Sex sex, Double age, Double weight, String breed) {
+        this.name = name;
+        this.type = type;
+        this.sex = sex;
+        this.age = age;
+        this.weight = weight;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public Double getAge() {
+        return age;
+    }
+
+    public void setAge(Double age) {
+        this.age = age;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", type=" + type +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
 }
