8e39045e711a8782db53a6f0afd4cd57d81ddde0
gustcustodio <gustcustodio@gmail.com>
2025-05-11 12:50:15 -0300

    feat: lembrete todo

--- app/Program.java
+++ app/Program.java
@@ -3,6 +3,7 @@ package app;
 import app.menu.MainMenu;
 
 public class Program {
+    // todo Ajustar edição de arquivo: 5 - 7 anos anos //
     public static void main(String[] args) {
         MainMenu.initialMenu();
     }
