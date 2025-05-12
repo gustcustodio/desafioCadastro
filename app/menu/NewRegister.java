b0aeb627dbc1d2c78378bd27f80809e5e1aa2cc9
gustcustodio <gustcustodio@gmail.com>
2025-05-12 06:19:11 -0300

    refactor: refatorando savePetFile para seguir o mesmo padrão de savePetChanges

--- app/menu/NewRegister.java
+++ app/menu/NewRegister.java
@@ -6,6 +6,11 @@ import app.model.enums.Sex;
 import app.model.enums.Type;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -54,32 +59,43 @@ public class NewRegister {
         String dateAndTimeFormatted = timeNow.format(timeFormatter);
 
         String fileName = dateAndTimeFormatted + "-" + formattedName + ".txt";
+        Path filePath = Path.of("app/petsCadastrados", fileName);
 
         try {
-            File file = new File("app/petsCadastrados/" + fileName);
-            if (file.createNewFile()) {
-                System.out.println("\uD83D\uDCC4 Um arquivo com as informações do pet foi criado.");
-            }
-            try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-                bw.write("1 - " + pets.get(index).getPetName());
-                bw.newLine();
-                bw.write("2 - " + pets.get(index).getPetType());
-                bw.newLine();
-                bw.write("3 - " + pets.get(index).getPetSex());
-                bw.newLine();
-                bw.write("4 - " + pets.get(index).getPetAddress().toString());
-                bw.newLine();
-                bw.write("5 - " + pets.get(index).getPetAge() + " anos");
-                bw.newLine();
-                bw.write("6 - " + pets.get(index).getPetWeight() + "kg");
-                bw.newLine();
-                bw.write("7 - " + pets.get(index).getPetBreed());
+            Files.createDirectories(filePath.getParent());
+            Files.createFile(filePath);
+            System.out.println("\uD83D\uDCC4 Um arquivo com as informações do pet foi criado.");
+
+            try (BufferedWriter writer = Files.newBufferedWriter(
+                    filePath,
+                    StandardCharsets.UTF_8,
+                    StandardOpenOption.WRITE)) {
+
+                Pet pet = pets.get(index);
+                writer.write("1 - " + pet.getPetName());
+                writer.newLine();
+                writer.write("2 - " + pet.getPetType());
+                writer.newLine();
+                writer.write("3 - " + pet.getPetSex());
+                writer.newLine();
+                writer.write("4 - Rua " +
+                        pet.getPetAddress().getStreetOfAdress() + ", " +
+                        pet.getPetAddress().getNumberOfAddress() + ", " +
+                        pet.getPetAddress().getCityOfAdress());
+                writer.newLine();
+                writer.write("5 - " + pet.getPetAge() + " anos");
+                writer.newLine();
+                writer.write("6 - " + pet.getPetWeight() + "kg");
+                writer.newLine();
+                writer.write("7 - " + pet.getPetBreed());
+
             } catch (IOException e) {
                 System.out.println("⚠️️ Ocorreu um erro na escrita do arquivo");
             }
+        } catch (FileAlreadyExistsException e) {
+            System.out.println("⚠️️ O arquivo já existe.");
         } catch (IOException e) {
             System.out.println("⚠️️ Ocorreu um erro na criação do arquivo.");
         }
     }
-
 }
