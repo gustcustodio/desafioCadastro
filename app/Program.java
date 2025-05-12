8de883e5a2b7c4c21ff51444401311c31c448563
gustcustodio <gustcustodio@gmail.com>
2025-05-12 06:24:29 -0300

    chore: finalizando o desafio

--- app/Program.java
+++ app/Program.java
@@ -3,7 +3,6 @@ package app;
 import app.menu.MainMenu;
 
 public class Program {
-    // todo Ajustar edição de arquivo: 5 - 7 anos anos //
     public static void main(String[] args) {
         MainMenu.initialMenu();
     }
